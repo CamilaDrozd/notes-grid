1ab6fd5fe7264453a281e472e0288e8ae1ac54aa
Camila <camilad.palma@gmail.com>
2026-01-22 14:04:35 -0300

    Criando componente new-note e interface Nota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewNoteComponent } from './components/new-note/new-note.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NewNoteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/models/nota.models.ts
@@ -0,0 +1,8 @@
+export interface Nota{
+  id: number;
+  title: string;
+  conteudo: string;
+  dataCriacao: Date;
+  dataAtualizacao: Date;
+  //categoria: enum de tipos: Work, Travel, Reading, Personal...
+}
