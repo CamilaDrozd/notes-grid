23c86dadf4c3f7455a83eaa9780550600f68b687
Camila <camilad.palma@gmail.com>
2026-01-26 16:23:10 -0300

    Adicionando metodo de salvar nota, e estilizando o new-note.scss

--- src/app/components/new-note/new-note.component.ts
+++ src/app/components/new-note/new-note.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { Nota } from '../../models/nota.models';
 import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-note',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, FormsModule],
   templateUrl: './new-note.component.html',
   styleUrl: './new-note.component.scss'
 })
 export class NewNoteComponent {
 
+  novaNota = {
+    titulo: '',
+    conteudo: ''
+  }
+
+  salvarNota(){
+    console.log('Titulo capturado:', this.novaNota.titulo);
+    console.log('Texto capturado:', this.novaNota.conteudo);
+
+  }
 }
